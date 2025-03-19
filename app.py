50fd04af95ae3b2f766a6e685ea826a20ddba34d
joao camilo <jlf.camilo@icloud.com>
2025-03-19 16:44:49 -0400

    fix: fixed main endpoint return.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Ocean Brasil! Eu amo o curso de dev ops</p>"
+    return "<p>Ocean Brasil!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
